bc140a3594186b10f0763caa84d58f37fe3b379a
Morgan Ogola <morganogola@gmail.com>
2025-03-25 14:30:42 +0300

    First commit

---
+++ src/main/java/com/example/inua_mkulima_backend/InuaMkulimaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.inua_mkulima_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InuaMkulimaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InuaMkulimaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inua_mkulima_backend/InuaMkulimaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inua_mkulima_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InuaMkulimaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
